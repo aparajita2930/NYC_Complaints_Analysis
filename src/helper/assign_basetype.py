5680119c80bdafaa2b3e66c24e67576c0bb3a395
Akshay <akshay.kalia@nyu.edu>
2017-04-07 15:28:43 -0400

    Fixing base type names

--- src/helper/assign_basetype.py
+++ src/helper/assign_basetype.py
@@ -6,11 +6,11 @@ def raise_(ex):
     raise ex
 
 basetypes = OrderedDict()
-basetypes[int] = int
-basetypes[float] = float
-basetypes['date'] = (lambda x: datetime.strptime(x, "%m/%d/%Y"))
-basetypes['datetime'] = (lambda x: datetime.strptime(x, "%m/%d/%Y %I:%M:%S %p"))
-basetypes['geo_code'] = (lambda x: 1 if ( re.search("^\((\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)\)$", x)) else raise_(ValueError('Error')))
+basetypes['INT'] = int
+basetypes['DECIMAL'] = float
+basetypes['DATE'] = (lambda x: datetime.strptime(x, "%m/%d/%Y"))
+basetypes['DATETIME'] = (lambda x: datetime.strptime(x, "%m/%d/%Y %I:%M:%S %p"))
+#basetypes['GEO_CODE'] = (lambda x: 1 if ( re.search("^\((\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)\)$", x)) else raise_(ValueError('Error')))
 
 def get_basetype(val):
   for basetype, try_type in basetypes.items():
@@ -19,7 +19,7 @@ def get_basetype(val):
       return basetype
     except ValueError:
       continue
-  return str
+  return 'TEXT'
 
 values = ['10.3', '12/31/2010', '12/31/2010 03:10:09 AM', '15', 'hello', "(40.65662129596871, -73.95806621423951)" ]
 for val in values:
