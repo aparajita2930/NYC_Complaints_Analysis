0dbe89c33a861c74274dc3347aa8c72254d548a7
Aparajita <ac5901@nyu.edu>
2017-04-06 23:06:01 -0400

    Fixed the script to handle quoted commas in csv and save the distribution files

--- use_cases/comp_count.py
+++ use_cases/comp_count.py
@@ -4,6 +4,7 @@ import sys
 from operator import add
 from pyspark import SparkContext
 from csv import reader
+import re
 
 reload(sys)
 sys.setdefaultencoding('utf-8')
@@ -13,30 +14,29 @@ if __name__ == "__main__":
         print("Usage: location_distribution <file>",  file=sys.stderr)
         exit(-1)
     sc = SparkContext()
-    lines = sc.textFile(sys.argv[1], 1)
-    lines = lines.map(lambda x: x.split(","))
+    lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
+    #lines = lines.map(lambda x: x.split(","))
+    lines = lines.mapPartitions(lambda x: reader(x))    
     
-    
-    
-    count_locations = lines.map(lambda line : (line[7].encode('utf-8'), 1)) \
+    count_locations = lines.map(lambda line : (line[7].encode('utf-8').strip(), 1)) \
                       .reduceByKey(add) \
-                      .map(lambda x, y : "%s\t%s" % (x, y)) 
+                      .map(lambda x: "%s\t%s" % (x[0], x[1])) 
     
     
-    count_bour = lines.map(lambda line : (line[23].encode('utf-8'), 1)) \
+    count_bour = lines.map(lambda line : (line[23].encode('utf-8').strip(), 1)) \
                       .reduceByKey(add) \
-                      .map(lambda x, y : "%s\t%s" % (x, y))
+                      .map(lambda x: "%s\t%s" % (x[0], x[1]))
     
     
-    count_comp_by_location = lines.map(lambda line : ((line[5], line[7]).encode('utf-8'), 1)) \
+    count_comp_by_location = lines.map(lambda line : ((line[5].encode('utf-8').strip(), line[7].encode('utf-8').strip()), 1)) \
                       .reduceByKey(add) \
-                      .map(lambda x, y : "%s\t%s" % (x, y)) 
+                      .map(lambda x: "%s\t%s" % (x[0], x[1])) 
                                 
     
                             
-    count_comp_by_city = lines.map(lambda line : ((line[5], line[16]).encode('utf-8'), 1)) \
+    count_comp_by_city = lines.map(lambda line : ((line[5].encode('utf-8'), line[16].encode('utf-8')), 1)) \
                       .reduceByKey(add) \
-                      .map(lambda x, y : "%s\t%s" % (x, y))  
+                      .map(lambda x: "%s\t%s" % (x[0], x[1]))  
                               
     count_bour.saveAsTextFile("bour_dist.out")                    
     count_comp_by_city.saveAsTextFile("types_by_city.out")
