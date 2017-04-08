145b907ff49b716f13898d6f736276f9944c7ef5
Akshay <akshay.kalia@nyu.edu>
2017-04-08 11:33:02 -0400

    Adding all files for dates

--- src/column_summary/3_closed_date.py
+++ src/column_summary/3_closed_date.py
@@ -12,32 +12,32 @@ from date_common import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
-def get_label(value, basetype):
+def get_label(value, basetype, date_range):
 	if basetype == "TEXT":
 		if not value:
 			return "NULL"
 		else:
 			return "INVALID"
-	elif (basetype == "DATETIME" and date_in_valid_time_range(value)):
+	elif (basetype == "DATETIME" and date_in_valid_time_range(value) and date_more_than(value,date_range)):
 		return "VALID"
 	else:
 		return "INVALID"
 
 
-def create_labels(value):
+def create_labels(value, created_date, due_date, resolution_due_date):
 	basetype = get_basetype(value)
 	semantictype = None
-	label = get_label(value, basetype)
+	label = get_label(value, basetype,[created_date,due_date,resolution_due_date])
 	return "%s %s %s" % (basetype, semantictype, label)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: 2_created_date.py <file>",  file=sys.stderr)
+        print("Usage: 3_closed_date.py <file>",  file=sys.stderr)
         exit(-1)
     sc = SparkContext()
     lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
     #lines = lines.map(lambda x: x.split(","))
     lines = lines.mapPartitions(lambda x: reader(x)) 
-    details_created_date = lines.map(lambda line : ("%s\t%s" % (line[2].encode('utf-8').strip(), create_labels(line[2].encode('utf-8').strip()))))
-    details_created_date.saveAsTextFile("2_created_date_details.out")
+    details_closed_date = lines.map(lambda line : ("%s\t%s" % (line[3].encode('utf-8').strip(), create_labels(line[3].encode('utf-8').strip(),line[2].encode('utf-8').strip(),line[21].encode('utf-8').strip(),line[22].encode('utf-8').strip()))))
+    details_closed_date.saveAsTextFile("3_created_date_details.out")
     sc.stop()
--- src/helper/date_common.py
+++ src/helper/date_common.py
@@ -1,16 +1,24 @@
 from datetime import datetime
 
-# Checks that the date is less than the other date
-def date_less_than(value, other):
+# Checks that the date is less than the other dates array
+def date_less_than(value, others):
 	dt = datetime.strptime(value, "%m/%d/%Y %I:%M:%S %p")
-	dt2 = datetime.strptime(other, "%m/%d/%Y %I:%M:%S %p")
-	return True if dt < dt2 else False
+	for other in others:
+		if other:
+			dt2 = datetime.strptime(other, "%m/%d/%Y %I:%M:%S %p")
+			if dt > dt2:
+				return False
+	return True
 
-# Checks that the date is more than the other date
-def date_more_than(value, other):
+# Checks that the date is more than the other dates array
+def date_more_than(value, others):
 	dt = datetime.strptime(value, "%m/%d/%Y %I:%M:%S %p")
-	dt2 = datetime.strptime(other, "%m/%d/%Y %I:%M:%S %p")
-	return True if dt > dt2 else False
+	for other in others:
+		if other:
+			dt2 = datetime.strptime(other, "%m/%d/%Y %I:%M:%S %p")
+			if dt < dt2:
+				return False
+	return True
 
 def date_in_valid_time_range(value):
 	dt = datetime.strptime(value, "%m/%d/%Y %I:%M:%S %p")
