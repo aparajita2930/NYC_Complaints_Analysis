676b5ad0c460a7016633b9986a58c62610973076
Aparajita <ac5901@nyu.edu>
2017-04-08 02:30:14 -0400

    Removing header from output file

--- src/join_csvs.py
+++ src/join_csvs.py
@@ -8,9 +8,11 @@ if len(sys.argv) != 3:
 	exit(-1)
 sc = SparkContext()
 lines_2010_2016 = sc.textFile(sys.argv[1], 1)
+lines_2010_2016_header = lines_2010_2016.first()
 lines_2009 = sc.textFile(sys.argv[2], 1)
 lines_2009_header = lines_2009.first()
 
+lines_2010_2016 = lines_2010_2016.filter(lambda x: x != lines_2010_2016_header)
 lines_2009 = lines_2009.filter(lambda x: x != lines_2009_header)
 
 #lines_2009 = lines_2009.mapPartitionsWithIndex(lambda (idx, iter): (iter.drop(1) if (idx == 0) else iter))
