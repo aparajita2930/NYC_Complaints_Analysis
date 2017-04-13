859f298959c438870f3a674038396f04d9ae5c53
Akshay <akshay.kalia@nyu.edu>
2017-04-13 15:34:05 -0400

    Adding column summary for Address Landmark Street Name School Address

---
+++ src/column_summary/18_landmark.py
@@ -0,0 +1,43 @@
+from __future__ import print_function
+
+import sys
+import os
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+import re
+
+def get_semantictype(basetype, label):
+	if label == 'VALID':
+		return 'Landmark'
+	else:
+		return 'None'
+
+def get_label(val, basetype):
+	if basetype == 'TEXT':
+		if val is None or len(val.strip()) == 0 or val.upper() in ['UNSPECIFIED', 'NA', 'N/A', 'N?A', 'NA/']:
+			return 'NULL'
+		else:
+			return 'VALID'
+	else:
+		return 'INVALID'
+
+
+def create_labels(value):
+	basetype = get_basetype(value)
+	label = get_label(value, basetype)
+	semantictype = get_semantictype(basetype, label)
+	return "%s %s %s" % (basetype, semantictype, label)
+
+if __name__ == "__main__":
+	if len(sys.argv) != 2:
+		print("Usage: 18_landmark <file>",  file=sys.stderr)
+		exit(-1)
+	sc = SparkContext()
+	sc.addFile("src/helper/assign_basetype.py")
+	from assign_basetype import *
+	lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
+	lines = lines.mapPartitions(lambda x: reader(x)) 
+	details = lines.map(lambda line : ("%s\t%s" % (line[17].encode('utf-8').strip(), create_labels(line[17].encode('utf-8').strip()))))
+	details.saveAsTextFile("18_landmark_details.out")
+	sc.stop()
---
+++ src/column_summary/34_school_address.py
@@ -0,0 +1,46 @@
+from __future__ import print_function
+
+import sys
+import os
+from operator import add
+from pyspark import SparkContext
+from csv import reader
+
+sc = SparkContext()
+sc.addFile("src/helper/assign_basetype.py")
+from assign_basetype import *
+all_lines = sc.textFile("/user/ac5901/street_name.csv",1)
+valid_types = all_lines.map(lambda x: x).collect()
+
+def get_label(val, basetype):
+	val = val.upper()
+	if basetype == 'TEXT':
+		if val is None or len(val.strip()) == 0 or val in ['UNSPECIFIED', 'NA', 'N/A', 'N?A', 'NA/']:
+			return 'NULL'
+		elif any(ext in val for ext in valid_types):
+			return 'VALID'
+		else:
+			return 'INVALID'
+	else:
+		return 'INVALID'
+
+def get_semantictype(basetype, label):
+	if label == 'NULL' or label == 'VALID':
+		return 'ADDRESS'
+	else:
+		return 'NONE'
+
+def create_labels(value):
+	basetype = get_basetype(value)
+	label = get_label(value, basetype)
+	semantictype = get_semantictype(basetype, label)
+	return "%s %s %s" % (basetype, semantictype, label)
+
+if len(sys.argv) != 2:
+	print("Usage: 34_school_address <file>",  file=sys.stderr)
+	exit(-1)
+lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
+lines = lines.mapPartitions(lambda x: reader(x))
+details = lines.map(lambda line : ("%s\t%s" % (line[33].encode('utf-8').strip(), create_labels(line[33].encode('utf-8').strip()))))
+details.saveAsTextFile("34_school_address_summary.out")
+sc.stop()
