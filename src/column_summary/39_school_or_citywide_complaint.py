93a35483edb490c9f5c90c6453b2450fd09ab824
Akshay <akshay.kalia@nyu.edu>
2017-04-17 00:54:18 -0400

    Defect Fixes

--- src/column_summary/39_school_or_citywide_complaint.py
+++ src/column_summary/39_school_or_citywide_complaint.py
@@ -24,27 +24,24 @@ def check_entry(val):
 	else:
 		return 'INVALID'
 
-def get_semantictype(val0, val1, basetype):
+def get_semantictype(val0, basetype):
 	chk = check_entry(val0)
 	if chk == 'NULL' or chk == 'VALID':
 		return 'school_or_citywide_complaint_indicator'
 	else:
 		return 'None'
 
-def col_details(val0, val1):
+def col_details(val0):
 	basetype = get_basetype(val0)
-	semantictype = get_semantictype(val0, val1, basetype)
+	semantictype = get_semantictype(val0, basetype)
 	validity = check_entry(val0)
-	return (val0, basetype, semantictype, validity)
+	return "%s %s %s" % (basetype, semantictype, validity)
 
 if len(sys.argv) != 2:
 	print("Usage: 39_school_or_citywide_complaint <file>",  file=sys.stderr)
 	exit(-1)
 lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
 lines = lines.mapPartitions(lambda x: reader(x))
-
-details = lines.map(lambda line : (line[38].encode('utf-8').strip(), 1))
-
-details.map(lambda x: "%s\t%s %s %s" % (x[0], x[1], x[2], x[3])).saveAsTextFile("39_details.out")
-
+details = lines.map(lambda line : ("%s\t%s" % (line[38].encode('utf-8').strip(), col_details(line[38].encode('utf-8').strip()))))
+details.saveAsTextFile("39_details.out")
 sc.stop()
