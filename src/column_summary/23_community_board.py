5dd9d8d359b99331c46171c86bae5e423ed00e16
Akshay <akshay.kalia@nyu.edu>
2017-04-16 23:42:30 -0400

    Defect Fixes

--- src/column_summary/23_community_board.py
+++ src/column_summary/23_community_board.py
@@ -24,27 +24,25 @@ def check_cb(val):
 	else:
 		return 'INVALID'
 
-def get_semantictype(val0, val1, basetype):
+def get_semantictype(val0, basetype):
 	chk = check_cb(val0)
 	if chk == 'NULL' or chk == 'VALID':
 		return 'community_board'
 	else:
 		return 'None'
 
-def col_details(val0, val1):
+def col_details(val0):
 	basetype = get_basetype(val0)
-	semantictype = get_semantictype(val0, val1, basetype)
+	semantictype = get_semantictype(val0, basetype)
 	validity = check_cb(val0)
-	return (val0, basetype, semantictype, validity)
+	return "%s %s %s" % (basetype, semantictype, validity)
 
 if len(sys.argv) != 2:
 	print("Usage: 23_community_board <file>",  file=sys.stderr)
 	exit(-1)
 lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
 lines = lines.mapPartitions(lambda x: reader(x))
-
-details = lines.map(lambda line : (line[22].encode('utf-8').strip(), 1))
-
-details.map(lambda x: "%s\t%s %s %s" % (x[0], x[1], x[2], x[3])).saveAsTextFile("23_details.out")
+details = lines.map(lambda line : ("%s\t%s" % (line[22].encode('utf-8').strip(), col_details(line[22].encode('utf-8').strip()))))
+details.saveAsTextFile("23_details.out")
 
 sc.stop()
