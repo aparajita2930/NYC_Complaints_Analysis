2d936428588a9963386c1632129516be8c98a1ed
Akshay <akshay.kalia@nyu.edu>
2017-04-23 00:47:21 -0400

    bug fix

--- use_cases/zip_date_type_distribution.py
+++ use_cases/zip_date_type_distribution.py
@@ -14,7 +14,7 @@ lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
 lines = lines.mapPartitions(lambda x: reader(x)) 
 date_zip_complaint_type_dist = lines.map(lambda line : ((line[1].strip().encode('utf-8'),line[8].encode('utf-8'), line[5].encode('utf-8')), 1)) \
 				.reduceByKey(add) \
-				.sortBy(lambda x: (-x[1], x[0][0], x[0][1],x[0][2])) \
+				.sortBy(lambda x:x[1], False) \
 				.take(2000) \
 				.map(lambda x: "%s, %s, %s\t%s" % (x[0][0], x[0][1], x[0][2], x[1]))
 date_zip_complaint_type_dist.saveAsTextFile("date_zip_complaint_type_dist.out")
