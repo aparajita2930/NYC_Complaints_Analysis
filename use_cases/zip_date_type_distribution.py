7ab47c038bc08c42a649f47a87ff40b512571624
Akshay <akshay.kalia@nyu.edu>
2017-04-28 18:58:16 -0400

    Add output file

--- use_cases/zip_date_type_distribution.py
+++ use_cases/zip_date_type_distribution.py
@@ -13,9 +13,10 @@ sc = SparkContext()
 lines = sc.textFile(sys.argv[1], 1, use_unicode=False)
 lines = lines.mapPartitions(lambda x: reader(x)) 
 date_zip_complaint_type_dist = lines.map(lambda line : ((line[1].strip().encode('utf-8'),line[8].encode('utf-8'), line[5].encode('utf-8')), 1)) \
+				.filter(lambda ((d,z,t),c) : z and 'HEAT' not in t) \
 				.reduceByKey(lambda x,y: x + y) \
 				.sortBy(lambda x:x[1], False) \
-				.take(2000)
+				.take(500)
 				#.map(lambda x: "%s, %s, %s\t%s" % (x[0][0], x[0][1], x[0][2], x[1]))
 count = sc.parallelize(date_zip_complaint_type_dist)
 date_zip_complaint_type_dist = count.map(lambda (x,y): "%s\t%s" % (x,y))
